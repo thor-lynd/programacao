e914215f2874f96ca22a5d25ac3bec962b148662
thor-lynd <thor.oliveira@fatec.sp.gov.br>
2025-02-19 21:03:49 -0300

    Update exercicio1.java

--- exercicio1.java
+++ exercicio1.java
@@ -5,22 +5,20 @@ public class exercicio1{
     public static void main(String[] args) {
     
         Scanner scanner1 = new Scanner(System.in);
+
         
-        
+        int resultado = 0; 
+
         System.out.print("Quantos anos você tem?: ");
         int anos = scanner1.nextInt();
         System.out.print("Quantos meses você tem:? ");
         int meses = scanner1.nextInt(); 
         System.out.print("Quantos dias você tem?");
+        int dias = scanner1.nextInt();
+        resultado = anos * 365 + meses * 30 + dias;
+        System.out.println("você tem");
+        System.out.print (resultado);
         
-        System.out.print("você tem", anos, "dias");
-
-    
-
-     
+        scanner1.close();
     }
-    
-
-
-
 }
