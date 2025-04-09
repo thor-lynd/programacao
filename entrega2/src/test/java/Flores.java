05d2c1a3dfa9e914326e37303474e32cb8110f05
thor-lynd <thor.oliveira@fatec.sp.gov.br>
2025-04-09 19:56:09 -0300

    entrega2 refeita
    refiz para relembrar

---
+++ entrega2/src/test/java/Flores.java
@@ -0,0 +1,24 @@
+public class Flores {
+    private String nome;
+    private String cor;
+    private int altura;
+
+    public Flores(String nome, String cor, int altura) {
+        this.nome = nome;
+        this.cor = cor;
+        this.altura = altura;
+    }
+
+    public void florescer() {
+        System.out.println("A flor " + nome + " de cor " + cor + " está florescendo!");
+    }
+
+    public void crescer(int crescimento) {
+        altura += crescimento;
+        System.out.println("A flor " + nome + " cresceu. Nova altura: " + altura + " cm.");
+    }
+
+    public void murchar() {
+        System.out.println("A flor " + nome + " de cor " + cor + " murchou.");
+    }
+}
