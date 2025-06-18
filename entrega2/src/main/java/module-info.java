a4e375bea679d140f868bfe21dd4851faeeae872
thor-lynd <thor.oliveira@fatec.sp.gov.br>
2025-06-18 16:29:16 -0300

    Delete entrega2 directory

--- entrega2/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.entrega2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens org.example.entrega2 to javafx.fxml;
-    exports org.example.entrega2;
-}
